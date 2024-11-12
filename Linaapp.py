e83f7fe1ee5bff9e01fe2557d571eb05bc07a05e
Lina Yonan <147035881+Linayonan@users.noreply.github.com>
2024-11-12 22:22:34 +0100

    Update Linaapp.py

--- Linaapp.py
+++ Linaapp.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 API_URL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
 
-def fetch_temperature():
+def get_temperature():
     try:
         response = requests.get(API_URL)
         response.raise_for_status()  
         data = response.json()
         
-        for timeseries in data.get('timeSeries', []):
+        for time_series in data.get('timeSeries', []):
             for param in time_series.get("parameters", []):
                 if param['name'] == 't': 
                     return param["values"][0] 
@@ -36,4 +36,4 @@ def temperature_api():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080, debug=True)
