fea93a4ef82b31e8a520ee689e82960fb2753f2b
Lina Yonan <147035881+Linayonan@users.noreply.github.com>
2024-12-03 09:56:39 +0100

    Update Linaapp.py

--- Linaapp.py
+++ Linaapp.py
@@ -3,23 +3,19 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return'This is Flask python web app'
-
 API_URL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
 
 def get_temperature():
     try:
         response = requests.get(API_URL)
-        response.raise_for_status()  
+        response.raise_for_status()
         data = response.json()
         
         for time_series in data.get('timeSeries', []):
             for param in time_series.get("parameters", []):
-                if param['name'] == 't': 
-                    return param["values"][0] 
-      
+                if param['name'] == 't':  # Temperatur
+                    return param["values"][0]
+        return None
     except requests.RequestException as e:
         print("Error fetching data:", e)
         return None
@@ -31,12 +27,11 @@ def index():
         return render_template('index.html', temperature=temperature)
     else:
         return "Failed to fetch temperature data."
-        
+
 @app.route('/api/temperature')
-def temperature_api(): 
+def temperature_api():
     temperature = get_temperature()
     return jsonify({"temperature": temperature})
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
