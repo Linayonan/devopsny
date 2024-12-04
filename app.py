41c202f7a1a72c0a0384fef9427ae47b00c266af
Lina Yonan <147035881+Linayonan@users.noreply.github.com>
2024-12-04 14:14:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask
 import requests
-from datetime import datetime
 
-app = Flask(name)
+
+app = Flask(__name__)
 
 @app.route("/")
 def fetchweather():
-
     apiurl = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0686/lat/59.3293/data.json"
     headers = {
         "Accept": "application/json",
         "User-Agent": "weather-app",
     }
     response = requests.get(apiurl, headers=headers)
-    if response.statuscode == 200:
+    if response.status_code == 200:
         data = response.json()
         time_series = data["timeSeries"][0]
         parameters = time_series["parameters"]
@@ -37,6 +36,5 @@ def fetchweather():
     else:
         return "Failed to fetch data from SMHI API", response.status_code
 
-if __name == "__main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
-
